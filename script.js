93e88ba856601ecf59fcd53c88110f6a4dc63682
sadbatya <sadbatya@mail.ru>
2023-07-02 22:04:08 +0200

    feat: adaptive slider

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-let offset = 0 //смещение от левого края
-const sliderLine = document.querySelector('.slider-line')
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
 
-document.querySelector('.slider-next').addEventListener('click', () => {
-  offset += 256 
-  if(offset > 768){
-    offset = 0
-  }
-  sliderLine.style.left = -offset + 'px'
+function init() {
+  console.log('resize');
+  width = document.querySelector('.slider').offsetWidth;
+  sliderLine.style.width = width * images.length + 'px';
+  images.forEach((item) => {
+    item.style.width = width + 'px';
+    item.style.height = 'auto'
+  });
+  rollSlider()
+}
+window.addEventListener('resize', init)
+init();
+
+
+document.querySelector('.slider-next').addEventListener('click', () =>{
+  count++
+  if(count >= images.length){
+    count = 0
+  } 
+  rollSlider()
 })
 
-document.querySelector('.slider-prev').addEventListener('click', () => {
-  offset -= 256 
-  if(offset < 0){
-    offset = 768
-  }
-  sliderLine.style.left = -offset + 'px'
+document.querySelector('.slider-prev').addEventListener('click', () =>{
+  count--
+  if(count < 0){
+    count = images.length - 1
+  } 
+  rollSlider()
 })
+
+
+function rollSlider(){
+  sliderLine.style.transform = 'translate(-'+ count*width + 'px)';
+}
